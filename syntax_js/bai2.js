54df426c5333dd335d33361b1364c361ca423550
Nguyen Khoi <nguyenhongkhoikm@gmail.com>
2026-03-09 15:33:10 +0700

    kk

--- syntax_js/bai2.js
+++ syntax_js/bai2.js
@@ -5,3 +5,12 @@ console.log(name, old);
 console.log("next year");
 old++;
 console.log(name, old); 
+
+for(let i = 0; i < old; i++){
+    console.log("lan thu", i);
+}
+
+let k = 9;
+while (k--){
+    console.log(k);
+}
