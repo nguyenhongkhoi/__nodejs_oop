df511c6a1f944c0b353cb297b191a9ab464a48b4
Nguyen Khoi <nguyenhongkhoikm@gmail.com>
2026-03-13 07:16:00 +0700

    add

---
+++ syntax_js/b3.js
@@ -0,0 +1,6 @@
+const add = (a, b) =>{
+    let x = a +b;
+    return console.log(x);
+};
+
+add(2,3);
---
+++ syntax_js/testjs.js
@@ -0,0 +1,8 @@
+function greet(user){
+    return `Hello, ${user}`;
+}
+console.log(greet('kkk'));
+
+const add =(a, b) => a + b;
+
+console.log(add(2,3));
